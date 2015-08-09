da8e0866d82a544b74da65d861086427698e77a8
Erica Johnson <edj@uoregon.edu>
2015-08-09 16:14:52 -0700

    added simple bootstrap grid and moved index.html to root of project

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ var server = http.createServer(app);
 var bodyParser = require("body-parser");
 
 server.listen(8000);
-//testtest
 // these allow req.body (for handling HTTP requests on server-side):
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + "/client"));
+app.use(express.static(__dirname));
